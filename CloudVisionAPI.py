b1fb748a1deb457ea376860cda7f4a2b8a3df9f8
Jerry Sun <39817125+jerrys7@users.noreply.github.com>
2020-05-19 11:52:23 -0700

    Update CloudVisionAPI.py

--- CloudVisionAPI.py
+++ CloudVisionAPI.py
@@ -42,44 +42,19 @@ def async_detect_document(gcs_source_uri, gcs_destination_uri):
         print(blob.name)
 
     output = blob_list[0]
-    with open(file_source, encoding="UTF-8") as f:
-        data = json.load(f)
-    if 'responses' not in data:
-        return
-
-#     print('reading' + file_source)
-    s = ''
-    length = len(data['responses'])
-    print(length)
-    for i in range(length):
-        response = data['responses'][i]
-        s = s + fullTextAnnotation['text']
-        # print(s)
-
-    f = open(file_source + '.txt', 'w+', encoding="UTF-8")
-    f.write(s)
-    print('just printed file' + file_source)
-    f.close()
-
-response = json_format.Parse(
-        data, vision.types.AnnotateFileResponse())
-    i = 0;
-    while i < len(response):
+    json_string = output.download_as_string()
+    response = json_format.Parse(
+        json_string, vision.types.AnnotateFileResponse())
+    
+    response = json_format.Parse(
+            json_string, vision.types.AnnotateFileResponse())
     first_page_response = response.responses[0]
     annotation = first_page_response.full_text_annotation
-    
+  
     print(u'Full text:')
     print(annotation.text)
-    print(s)
 
-
-# 'KV_4_467.pdf', 'KV_4_468.pdf',
-# files = ['KV_4_469-2.pdf', 'KV_4_469-3.pdf', 'KV_4_469.pdf', 'KV_4_471.pdf', 'KV_4_472.pdf']
-# files = files + ['KV_4_474.pdf', 'KV_4_475.pdf', 'KV_4-185_1.pdf', 'KV_4-185_2.pdf', 'KV_4-186_1.pdf', 'KV_4-186_2.pdf']
-# files = files + ['KV_4-187_1.pdf', 'KV_4-188_2.pdf', 'KV_4-189_1.pdf', 'KV_4-189_2-2.pdf', 'KV_4-190_1.pdf', 'KV_4-190_2.pdf']
-# files = files + ['KV_4-191_2.pdf', 'KV_4-192_1.pdf', 'KV_4-192_3.pdf', 'KV_4-193_1.pdf', 'KV_4-193_3.pdf', 'KV_4-194_1.pdf', 'KV_4-194_2.pdf']
-# files = files + ['KV_4-195_2.pdf', 'KV_4-195_3.pdf', 'KV_4-196_1.pdf', 'KV_4-196_2.pdf', 'KV_4-196_3.pdf']
-
+files = 'file_name'
 source_bucket = 'gs://cambridgefive_pdfs/'
 destinations_bucket = 'gs://cambridgefive_pdfs/'
 i = 0
